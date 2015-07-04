b170a1e6a4299611e084cb337867769da60ea830
Ivar Stangeby <istangeby@gmail.com>
2015-07-04 04:24:02 +0200

    Changed number of iterations to 255

--- src/fractal_generator/fractal_gen.py
+++ src/fractal_generator/fractal_gen.py
@@ -5,13 +5,13 @@ import numpy as np
 
 OUT_FILE = 'generated_fractal.dat'
 
-RESOLUTION_X = 512
-RESOLUTION_Y = 512
-WINDOW_OF_REFERENCE_X = (-2, 1)
-WINDOW_OF_REFERENCE_Y = (-1, 1)
-
-NUMBER_OF_ITERATIONS = 30
+RESOLUTION_X = 2048
+RESOLUTION_Y = 2048
 COMPLEX_SEED = complex(-0.4, 0.6)
+WINDOW_OF_REFERENCE_X = (COMPLEX_SEED.real - 2, COMPLEX_SEED.real + 2)
+WINDOW_OF_REFERENCE_Y = (COMPLEX_SEED.imag - 2,COMPLEX_SEED.imag + 2)
+
+NUMBER_OF_ITERATIONS = 255
 COMPLEX_FUNCTION = lambda z, c: z*z + c
 BAILOUT_VALUE = 4
 
@@ -26,8 +26,8 @@ with open(OUT_FILE, 'w') as output:
     output.write('#number_of_iterations = %d\n' % NUMBER_OF_ITERATIONS)
     output.write('#complex_seed = %s\n' % str(COMPLEX_SEED))
 
-    for i, a in enumerate(COMPLEX_GRID_X):
-        for j, b in enumerate(COMPLEX_GRID_Y):
+    for i, b in enumerate(COMPLEX_GRID_Y):
+        for j, a in enumerate(COMPLEX_GRID_X):
             z = complex(a, b)
             z_new = z
             for n in range(NUMBER_OF_ITERATIONS):
